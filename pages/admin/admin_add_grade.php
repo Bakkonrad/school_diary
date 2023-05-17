5c2287845d8525b652f345e376b6f84de8b06923
budaala <budaala4@gmail.com>
2023-05-17 21:37:53 +0200

    added Konrad's code

---
+++ pages/admin/admin_add_grade.php
@@ -0,0 +1,268 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>KoalaSchool | Dodawanie ocen</title>
+
+  <!-- Google Font: Source Sans Pro -->
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
+  <!-- Font Awesome -->
+  <link rel="stylesheet" href="../../plugins/fontawesome-free/css/all.min.css">
+  <!-- Theme style -->
+  <link rel="stylesheet" href="../../dist/css/adminlte.min.css">
+</head>
+
+<body class="hold-transition layout-top-nav">
+  <div class="wrapper">
+    <!-- Navbar -->
+    <nav class="main-header navbar navbar-expand-md navbar-primary navbar-dark">
+      <div class="container">
+        <a href="admin_main.php" class="navbar-brand">
+          <span class="brand-text"><b>dziennik</b><br>lekcyjny</span>
+        </a>
+
+        <button class="navbar-toggler order-1" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse order-3" id="navbarCollapse">
+          <!-- Left navbar links -->
+          <ul class="navbar-nav">
+            <li class="nav-item">
+              <a href="admin_main.php" class="nav-link">Strona główna</a>
+            </li>
+            <li class="nav-item">
+              <a href="admin_edit_users.php" class="nav-link">Wyświetl lub edytuj użytkowników</a>
+            </li>
+            <li class="nav-item">
+              <a href="admin_add_user.php" class="nav-link">Dodawanie użytkownika</a>
+            </li>
+            <li class="nav-item">
+              <a href="admin_add_grade.php" class="nav-link">Dodawanie ocen</a>
+            </li>
+          </ul>
+        </div>
+
+        <!-- Right navbar links -->
+        <ul class="order-1 order-md-3 navbar-nav navbar-no-expand ml-auto">
+          <!-- SEARCH FORM -->
+          <form class="form-inline ml-0 ml-md-3">
+            <div class="input-group input-group-sm">
+              <input class="form-control form-control-navbar" type="search" placeholder="szukaj" aria-label="Search">
+              <div class="input-group-append">
+                <button class="btn btn-navbar" type="submit">
+                  <i class="fas fa-search"></i>
+                </button>
+              </div>
+            </div>
+          </form>
+          <!-- ACCOUNT ICON -->
+          <li class="nav-item">
+            <a href="admin_account.php" class="nav-link">
+              <i class="fa fa-solid fa-user-shield" style="color: #ffffff;"></i>
+            </a>
+          </li>
+        </ul>
+      </div>
+    </nav> <!-- /.navbar -->
+
+    <!-- Content Header (Page header) -->
+    <div class="content-wrapper">
+      <div class="content-header">
+        <div class="container">
+          <h1 class="m-0">Dodawanie ocen</h1>
+          <!-- <h1 class="m-0"> Strona główna <small>zalogowany</small></h1> -->
+        </div> <!-- /.container-fluid -->
+      </div> <!-- /.content-header -->
+
+      <!-- Main content -->
+      <div class="content">
+        <div class="container">
+          <div class="card">
+            <div class="card-header">
+              <h3 class="card-title">DataTable with default features</h3>
+            </div>
+            <!-- /.card-header -->
+            <div class="card-body">
+              <div id="example1_wrapper" class="dataTables_wrapper dt-bootstrap4">
+                <div class="row">
+                  <div class="col-sm-12 col-md-6">
+                    <div class="dt-buttons btn-group flex-wrap"> 
+                      <button class="btn btn-secondary buttons-copy buttons-html5" tabindex="0" aria-controls="example1" type="button">
+                        <span>Copy</span>
+                      </button> 
+                      <button class="btn btn-secondary buttons-csv buttons-html5" tabindex="0" aria-controls="example1" type="button">
+                        <span>CSV</span>
+                      </button> 
+                      <button class="btn btn-secondary buttons-excel buttons-html5" tabindex="0" aria-controls="example1" type="button">
+                        <span>Excel</span>
+                      </button> 
+                      <button class="btn btn-secondary buttons-pdf buttons-html5" tabindex="0" aria-controls="example1" type="button">
+                        <span>PDF</span>
+                      </button> 
+                      <button class="btn btn-secondary buttons-print" tabindex="0" aria-controls="example1" type="button">
+                        <span>Print</span>
+                      </button>
+                      <div class="btn-group">
+                        <button class="btn btn-secondary buttons-collection dropdown-toggle buttons-colvis" tabindex="0" aria-controls="example1" type="button" aria-haspopup="true" aria-expanded="false">
+                          <span>Column visibility</span>
+                          <span class="dt-down-arrow"></span>
+                        </button>
+                      </div>
+                    </div>
+                  </div>
+                  <div class="col-sm-12 col-md-6">
+                    <div id="example1_filter" class="dataTables_filter">
+                      <label>Search:<input type="search" class="form-control form-control-sm" placeholder="" aria-controls="example1"></label>
+                    </div>
+                  </div>
+                </div>
+                <div class="row">
+                  <div class="col-sm-12">
+                    <table id="example1" class="table table-bordered table-striped dataTable dtr-inline" aria-describedby="example1_info">
+                      <thead>
+                        <tr>
+                          <th class="sorting sorting_asc" tabindex="0" aria-controls="example1" rowspan="1" colspan="1" aria-sort="ascending" aria-label="Rendering engine: activate to sort column descending">Rendering engine</th>
+                          <th class="sorting" tabindex="0" aria-controls="example1" rowspan="1" colspan="1" aria-label="Browser: activate to sort column ascending">Browser</th>
+                          <th class="sorting" tabindex="0" aria-controls="example1" rowspan="1" colspan="1" aria-label="Platform(s): activate to sort column ascending">Platform(s)</th>
+                          <th class="sorting" tabindex="0" aria-controls="example1" rowspan="1" colspan="1" aria-label="Engine version: activate to sort column ascending">Engine version</th>
+                          <th class="sorting" tabindex="0" aria-controls="example1" rowspan="1" colspan="1" aria-label="CSS grade: activate to sort column ascending">CSS grade</th>
+                        </tr>
+                      </thead>
+                      <tbody>
+
+                        <tr class="odd">
+                          <td class="dtr-control sorting_1" tabindex="0">Gecko</td>
+                          <td>Firefox 1.0</td>
+                          <td>Win 98+ / OSX.2+</td>
+                          <td>1.7</td>
+                          <td>A</td>
+                        </tr>
+                        <tr class="even">
+                          <td class="dtr-control sorting_1" tabindex="0">Gecko</td>
+                          <td>Firefox 1.5</td>
+                          <td>Win 98+ / OSX.2+</td>
+                          <td>1.8</td>
+                          <td>A</td>
+                        </tr>
+                        <tr class="odd">
+                          <td class="dtr-control sorting_1" tabindex="0">Gecko</td>
+                          <td>Firefox 2.0</td>
+                          <td>Win 98+ / OSX.2+</td>
+                          <td>1.8</td>
+                          <td>A</td>
+                        </tr>
+                        <tr class="even">
+                          <td class="dtr-control sorting_1" tabindex="0">Gecko</td>
+                          <td>Firefox 3.0</td>
+                          <td>Win 2k+ / OSX.3+</td>
+                          <td>1.9</td>
+                          <td>A</td>
+                        </tr>
+                        <tr class="odd">
+                          <td class="sorting_1 dtr-control">Gecko</td>
+                          <td>Camino 1.0</td>
+                          <td>OSX.2+</td>
+                          <td>1.8</td>
+                          <td>A</td>
+                        </tr>
+                        <tr class="even">
+                          <td class="sorting_1 dtr-control">Gecko</td>
+                          <td>Camino 1.5</td>
+                          <td>OSX.3+</td>
+                          <td>1.8</td>
+                          <td>A</td>
+                        </tr>
+                        <tr class="odd">
+                          <td class="sorting_1 dtr-control">Gecko</td>
+                          <td>Netscape 7.2</td>
+                          <td>Win 95+ / Mac OS 8.6-9.2</td>
+                          <td>1.7</td>
+                          <td>A</td>
+                        </tr>
+                        <tr class="even">
+                          <td class="sorting_1 dtr-control">Gecko</td>
+                          <td>Netscape Browser 8</td>
+                          <td>Win 98SE+</td>
+                          <td>1.7</td>
+                          <td>A</td>
+                        </tr>
+                        <tr class="odd">
+                          <td class="sorting_1 dtr-control">Gecko</td>
+                          <td>Netscape Navigator 9</td>
+                          <td>Win 98+ / OSX.2+</td>
+                          <td>1.8</td>
+                          <td>A</td>
+                        </tr>
+                        <tr class="even">
+                          <td class="sorting_1 dtr-control">Gecko</td>
+                          <td>Mozilla 1.0</td>
+                          <td>Win 95+ / OSX.1+</td>
+                          <td>1</td>
+                          <td>A</td>
+                        </tr>
+                      </tbody>
+                      <tfoot>
+                        <tr>
+                          <th rowspan="1" colspan="1">Rendering engine</th>
+                          <th rowspan="1" colspan="1">Browser</th>
+                          <th rowspan="1" colspan="1">Platform(s)</th>
+                          <th rowspan="1" colspan="1">Engine version</th>
+                          <th rowspan="1" colspan="1">CSS grade</th>
+                        </tr>
+                      </tfoot>
+                    </table>
+                  </div>
+                </div>
+                <div class="row">
+                  <div class="col-sm-12 col-md-5">
+                    <div class="dataTables_info" id="example1_info" role="status" aria-live="polite">Showing 1 to 10 of 57 entries</div>
+                  </div>
+                  <div class="col-sm-12 col-md-7">
+                    <div class="dataTables_paginate paging_simple_numbers" id="example1_paginate">
+                      <ul class="pagination">
+                        <li class="paginate_button page-item previous disabled" id="example1_previous"><a href="#" aria-controls="example1" data-dt-idx="0" tabindex="0" class="page-link">Previous</a></li>
+                        <li class="paginate_button page-item active"><a href="#" aria-controls="example1" data-dt-idx="1" tabindex="0" class="page-link">1</a></li>
+                        <li class="paginate_button page-item "><a href="#" aria-controls="example1" data-dt-idx="2" tabindex="0" class="page-link">2</a></li>
+                        <li class="paginate_button page-item "><a href="#" aria-controls="example1" data-dt-idx="3" tabindex="0" class="page-link">3</a></li>
+                        <li class="paginate_button page-item "><a href="#" aria-controls="example1" data-dt-idx="4" tabindex="0" class="page-link">4</a></li>
+                        <li class="paginate_button page-item "><a href="#" aria-controls="example1" data-dt-idx="5" tabindex="0" class="page-link">5</a></li>
+                        <li class="paginate_button page-item "><a href="#" aria-controls="example1" data-dt-idx="6" tabindex="0" class="page-link">6</a></li>
+                        <li class="paginate_button page-item next" id="example1_next"><a href="#" aria-controls="example1" data-dt-idx="7" tabindex="0" class="page-link">Next</a></li>
+                      </ul>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <!-- /.card-body -->
+          </div>
+        </div> <!-- /.container-fluid -->
+      </div> <!-- /.content -->
+    </div> <!-- /.content-wrapper -->
+
+    <!-- Main Footer -->
+    <footer class="main-footer">
+      <!-- To the right -->
+      <div class="float-right d-none d-sm-inline">
+        KoalaSchool
+      </div>
+      <!-- Default to the left -->
+      <strong>Copyright &copy; 2023</strong> All rights reserved.
+    </footer>
+  </div>
+  <!-- ./wrapper -->
+
+
+  <!-- jQuery -->
+  <script src="../../plugins/jquery/jquery.min.js"></script>
+  <!-- Bootstrap 4 -->
+  <script src="../../plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <!-- AdminLTE App -->
+  <script src="../../dist/js/adminlte.min.js"></script>
+
+</body>
+
+</html>
