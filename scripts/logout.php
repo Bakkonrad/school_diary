5c7c60bc037ca4b1de00941a2e5375d09ae2f8f2
Bakkonrad <konri0908@gmail.com>
2023-05-18 22:28:26 +0200

    pulling branch from main

--- scripts/logout.php
+++ scripts/logout.php
@@ -3,8 +3,8 @@
 
     //ochrona przed wejsciem na strone przez url
     //$_SESSION['isLogged'] = false;
-    session_unset();
-    session_destroy();
+    session_unset(); //czyszczenie zmiennych sesyjnych
+    session_destroy();  //niszczenie sesji
     header('location: ../pages/index.php');
     exit();
 
